90e381ec2f6892e601c4cabeaf3b206ee92ab9e6
arturoruge <arturoruge@gmail.com>
2017-05-15 06:55:09 -0300

    secuenciales finalizados

---
+++ condicionales/3-3.py
@@ -0,0 +1,12 @@
+#Una compañía de seguros esta abriendo un departamento de finanzas y estableció un programa 
+#para captar clientes, que consiste en lo siguiente: Si el monto por el que se efectúa el seguro 
+#es menor que $50.000 la cuota a pagar será por el 3% del monto, y si el monto es mayor que $50.000 
+#la cuota a pagar será el 2 % del monto. La aseguradora desea determinar cuál será la cuota que debe pagar un cliente.
+
+montoSeguro = int(input("Valor del monto del seguro: "))
+if montoSeguro < 50000:
+	cuota = montoSeguro * 0.03
+else:
+	cuota = montoSeguro * 0.02
+
+print("la cuota a pagar es de: " + str(round(cuota,2)))
